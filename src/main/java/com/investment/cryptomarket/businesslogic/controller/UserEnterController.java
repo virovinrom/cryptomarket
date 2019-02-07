4622ad2191b62048108e1fc466252ab35c90a918
Vjaceslav <goodlord@inbox.lv>
2019-02-07 22:19:59 +0200

    REST

--- src/main/java/com/investment/cryptomarket/businesslogic/controller/UserEnterController.java
+++ src/main/java/com/investment/cryptomarket/businesslogic/controller/UserEnterController.java
@@ -1,17 +1,31 @@
 package com.investment.cryptomarket.businesslogic.controller;
 
 import com.investment.cryptomarket.businesslogic.builders.user.User;
-import com.investment.cryptomarket.businesslogic.database.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserEnterController {
+    private Logger logger = LoggerFactory.getLogger(UserEnterController.class);
 
-    @RequestMapping("/")
-    public User greeting(User user) {
+
+    @GetMapping("/")
+    public User showUser() {
+        User user = new User();
+        user.setLogin("testLogin");
         return user;
+    }
+
+    @PostMapping("/")
+    public void postLogin(@RequestBody User user) {
+
+        logger.info(user.getLogin());
     }
 
 
